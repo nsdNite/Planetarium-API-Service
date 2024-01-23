799bccaa856daea0e5053adfc065ce9824c09044
nsdNite <nsd7night@gmail.com>
2024-01-23 19:55:58 +0200

    Register models in admin, run migrations for typo mistakes

--- planetarium_api/admin.py
+++ planetarium_api/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from planetarium_api.models import (
+    AstronomyShow,
+    PlanetariumDome,
+    Reservation,
+    ShowSession,
+    ShowTheme,
+    Ticket
+)
+
+admin.site.register(AstronomyShow)
+admin.site.register(PlanetariumDome)
+admin.site.register(Reservation)
+admin.site.register(ShowSession)
+admin.site.register(ShowTheme)
+admin.site.register(Ticket)
