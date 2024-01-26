864085156da623cab1a4f0c4724d3e3b0f7fe65d
nsdNite <nsd7night@gmail.com>
2024-01-26 08:44:33 +0200

    Implement JWT Token auth
    Update settings with DRF settings
    Update core urls with user urls
    Update user urls for JWT

--- planetarium_service/settings.py
+++ planetarium_service/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    #"django.contrib.admin",
+    "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
@@ -125,3 +125,9 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    )
+}
