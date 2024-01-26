725ec4cd20dbee1fd6d18e4f4f9ed97885ae87c1
nsdNite <nsd7night@gmail.com>
2024-01-26 09:01:45 +0200

    Add JWT tokens lifetime info

--- planetarium_service/settings.py
+++ planetarium_service/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import os
+from datetime import timedelta
+
 from dotenv import load_dotenv
 
 from pathlib import Path
@@ -127,4 +129,10 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     )
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=10),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ROTATE_REFRESH_TOKENS": False,
 }
