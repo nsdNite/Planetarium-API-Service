96eff0c94b57bba72b56f1e8959a2d5949bb7976
nsdNite <nsd7night@gmail.com>
2024-01-29 12:25:20 +0200

    Update .gitignore, models
    Update .gitignore with only neccessary stuff
    Update models foreign key related names

--- planetarium_api/models.py
+++ planetarium_api/models.py
@@ -41,10 +41,13 @@ class PlanetariumDome(models.Model):
 class ShowSession(models.Model):
     astronomy_show = models.ForeignKey(
         AstronomyShow,
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        related_name="show_sessions"
     )
     planetarium_dome = models.ForeignKey(
-        PlanetariumDome, on_delete=models.CASCADE
+        PlanetariumDome,
+        on_delete=models.CASCADE,
+        related_name="show_sessions"
     )
     show_time = models.DateTimeField()
 
@@ -59,7 +62,8 @@ class Reservation(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        related_name="reservations"
     )
 
     class Meta:
@@ -73,10 +77,14 @@ class Ticket(models.Model):
     row = models.IntegerField()
     seat = models.IntegerField()
     show_session = models.ForeignKey(
-        ShowSession, on_delete=models.CASCADE, related_name="tickets"
+        ShowSession,
+        on_delete=models.CASCADE,
+        related_name="tickets"
     )
     reservation = models.ForeignKey(
-        Reservation, on_delete=models.CASCADE, related_name="tickets"
+        Reservation,
+        on_delete=models.CASCADE,
+        related_name="tickets"
     )
 
     @staticmethod
